76cb6e88d72a936aea520ebeba0b50780e945dfc
accalgebraofficehours <147888375+accalgebraofficehours@users.noreply.github.com>
2025-12-09 17:33:15 -0700

    Update h1.js

--- static/assets/js/h1.js
+++ static/assets/js/h1.js
@@ -167,7 +167,7 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Interstellar but incredibly fast. Made by the Poodleschool team.",
+  "This is interstellar, but modified to be incredibly fast! Made by the Poodleschool team.",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
